7557406c9925f3709e8c7f3a76aacc168dbcfc96
konrad <konrad@xapos.(none)>
2009-12-16 13:34:34 +0100

    init

---
+++ src/Main.java
@@ -0,0 +1,85 @@
+import vacuumcleaner.base.*;
+import vacuumcleaner.agents.*;
+import vacuumcleaner.environments.*;
+import java.io.File;
+import java.awt.Point;
+
+public class Main 
+{
+    /**
+     * Testet einen Evaluator direkt ohne Oberfläche. Die erreichte Performance und die durchlaufenen Felder werden angezeigt.
+     * @param environment Umgebung, für die der Agent die bestmögliche Performance bestimmen soll
+     * @param evaluator Agent, der die bestmögliche Performance bestimmt
+     */
+    private static void testConsole(EnvironmentBase environment, IEnvironmentEvaluator evaluator)
+    {
+            System.out.println("Teste Performance mit "+evaluator.toString());
+	    int bestPerformance=evaluator.getOptimalPerformanceAtFullKnowledge(environment);
+	    System.out.println("best mögliche Performance: "+bestPerformance);
+            System.out.print("Durchlaufene Felder: (");
+            boolean first=true;
+            for(Point p:evaluator.getVisitedPoints())
+            {
+                if(first)
+                    first=false;
+                else
+                    System.out.print(",");
+                System.out.print("[");
+                System.out.print(p.x);
+                System.out.print(",");
+                System.out.print(p.y);
+                System.out.print("]");
+            }
+            System.out.println(")");
+    }
+
+    /**
+     * Zeigt die übergebene Umgebung an und fügt die übergebenen Evaluatoren in das "Performanceschätzung"-Menü ein
+     * @param environment Anzuzeigende Umgebung
+     * @param evaluators Liste der Evaluatoren
+     */
+    private static void testAndView(EnvironmentBase environment, IEnvironmentEvaluator[] evaluators)
+    {
+    	VacuumCleanerControler.start(
+        	//Envrionment
+		environment,
+
+		//irgendein Agent - um den gehts ja nicht	
+                new ReflexAgent1D(), //geht nur von einer Dimension aus
+
+		//mögliche Evaluatoren - zu finden im Menü Performancetest
+		evaluators
+                );
+    }
+
+    public static void main(String[] args)
+    {   
+	try
+	{
+            //1. Umgebung für Performanceschätzung wählen
+            //   a) Auswürfeln lassen
+            //   EnvironmentBase env=new ObstacleRectangleEnvironment(20,20);
+            //   b) Um Verbesserungen des Evaluators zu messen, die selbe Umgebung aus Datei laden:
+            String fileName="problem_klein.env";
+            EnvironmentBase env=EnvironmentBase.load(new File(fileName));
+
+            //2. Anzeigen und Evaluator laufen lassen
+            //   a) nur Evaluator laufen lassen und Ergebnis auf der Konsole anzeigen
+            //testConsole(env,new SimulatedAnnealingEnvironmentEvaluator());
+
+            //   b) Umgebung in "normaler" Simulation anzeigen lassen, Liste von
+            //      Evaluatoren ist über Menü "Performanceschätzung" erreichbar
+	    testAndView(env,new IEnvironmentEvaluator[]
+				{
+                                        new OptimisticEnvironmentEvaluator(),
+					new SimulatedAnnealingEnvironmentEvaluator(),
+    				});
+
+        }
+	catch(Exception ex)
+	{
+		System.out.println("Problem beim Laden:");
+		System.out.println(ex.toString());
+	}
+    }
+}
---
+++ src/SimulatedAnnealingEnvironmentEvaluator.java
@@ -0,0 +1,103 @@
+import java.awt.Point;
+import java.util.ArrayList;
+import vacuumcleaner.base.*;
+
+/**
+ * Abschätzen der best möglichen Performance bei vollständigem Wissen zur
+ * Evaluation eines Agenten durch Simulated Annealing
+ */
+public class SimulatedAnnealingEnvironmentEvaluator implements IEnvironmentEvaluator
+{
+    /**
+     * Bestimmt ausgehend von der aktuellen Sitution des Agenten die 
+     * maximal erreichbare Performance oder schätzt diese ab. Weiterhin wird noch
+     * die Liste der dabei besuchten Dreckfelder gefüllt
+     * @param environment Umgebung, zu der die bestmögliche Performance bestimmt 
+     * werden soll
+     * @return abgeschätzte oder bestimmte bestmögliche Performance
+     */
+    public int getOptimalPerformanceAtFullKnowledge(EnvironmentBase environment)
+    {
+        //1. Liste der besuchten Dreckfelder zurücksetzen
+        visitedDirtPoints=new ArrayList<Point>();
+
+        //2. die Startposition besuchen wir in jedem Fall
+        visitedDirtPoints.add(environment.getAgentLocation());
+
+        //3. die erreichbare Performance bestimmen
+        // das ausschalten kostet in jedem Fall einen Punkt
+        int reachablePerf=-1;
+
+        //4. Am Ende ist noch die Entscheidung möglich, ob sich der Agent sofort ausschaltet
+        //   (1000 Punkte Strafe), oder vorher zur Heimatposition fährt, wenn das weniger
+        //   als diese 1000 Punkte kostet
+        int wrongTurnoffFee=1000;
+
+        //5. letztendlich werden die Dreckpositionen zählen, diese machen die Größe des Problems aus
+        //   für 2 potentielle Dreckpositionen kann jedoch schon im Voraus ein mögliches
+        //   Vorgehen bestimmt werden - man muss natürlich aufpassen, dass diese "speziellen" Dreckpositionen
+        //   später nicht doppelt gezählt werden:
+        //   a) Dreck am Anfang wird immer aufgesaugt, das bringt netto 99 Punkte (100 Punkte Bonus - 1 Punkt Aktion Saugen)
+        if(environment.containsDirt(environment.getAgentLocation().x, environment.getAgentLocation().y))
+            reachablePerf+=99;
+        //   b) Dreck am Ende, der wird aufgesaugt, wenn der Agent zum Ende geht. D.h. es sind 99 Punkte mehr
+        //      erreichbar, um diese erhöht sich aber auch die Strafe für falsches ausschalten
+        //      -> das Ganze darf natürlich nur einmal gezählt werden, wenn die Start- und die Home-Position identisch sind
+        if(!environment.getAgentLocation().equals(environment.getAgentHome()) &&
+                environment.containsDirt(environment.getAgentHome().x,environment.getAgentHome().y))
+        {
+            reachablePerf+=99;
+            wrongTurnoffFee+=99;
+        }
+
+        //6. Umgebung in einen Graphen umwandeln (4 Zustände / Blickrichtungen) pro Raum
+        //7. Entfernungen in Aktionen bestimmen:
+        //   a) von der AgentenPosition/Start-Blickrichtung
+        //      ->(i) zu allen Dreckpositionen (ausser denen unter (5.) genannten) mit allen dabei möglichen Blickrichtungen
+        //      ->(ii) zur nächsten der 4 möglichen Home-Positions-Blickrichtungen
+        //   b) von jeder Dreckposition (ausser denen unter (5. genannten) mit allen dabei möglichen Blickrichtungen
+        //      ->(i) zu allen anderen Dreckpositionen (ausser denen unter (5.) genannten) mit allen dabei möglichen Blickrichtungen
+        //      ->(ii) zur nächsten der 4 möglichen Home-Positions-Blickrichtungen
+
+        //8. Ein Suchzustand ist nun eine Liste von durchfahrenen Dreckpositionen (ausser (5.)), jede Dreckposition wird maximal 1x in genau einer Blickrichtung durchfahren
+        //   Diese Liste kann auch leer sein, dann wird keine Dreckposition angefahren.
+
+        //9. Die Performance (d.h. die zu maximierende Größe) bestimmt sich aus:
+        //   ->  Basis-Performance (reachablePerf)
+        //   ->+ 99*Anzahl der durchfahrenen Dreckpositionen
+        //   ->- kumulierte dabei verbrauchte Navigationsaktionen (Drehen, GoForward)
+        //   ->- Endgebühr (aus Entfernung der letzten durchfahrenen Dreckposition oder der Start-Position, falls kein Dreck angefahren wird: lastDirtHomeDist)
+        //       a) lastDirtHomeDist>wrongTurnoffFee
+        //          -> es kostet mehr dahin als wrongTurnoffFee (meist =1000), daher lieber die Strafe in Kauf nehmen und gleich abschalten
+        //          -> Endgebühr=wrongTurnoffFee
+        //          -> Home-Position nicht in die Liste der besuchten Felder aufnehmen
+        //       b) lastDirtHomeDist<=wrongTurnoffFee
+        //          -> Hinfahren lohnt sich
+        //          -> Endgebühr=lastDirtHomeDist
+        //          -> Home-Position in die Liste der besuchten Felder aufnehmen
+
+        return reachablePerf;
+    }
+
+    /**
+     * Liste, der interessanten Punkte bei der letzten Schätzung, d.h. die Startposition des Agentne,
+     * Liste der gesäuberten Dreckfelder sowie evtl. die Home-Position
+     */
+    private ArrayList<Point> visitedDirtPoints;
+
+    /**
+     * Bestimmt nach einer Performancebestimmung die dabei besuchten Dreckfelder
+     * @return Liste der besuchten Dreckfelder (und anderer "interessanter" Punkte)
+     */
+    public ArrayList<Point> getVisitedPoints() 
+    {
+        return visitedDirtPoints;
+    }
+
+    @Override
+    public String toString()
+    {
+        //Anzeigename im Menü
+        return "Abschätzung per Simulated Annealing";
+    }
+}
